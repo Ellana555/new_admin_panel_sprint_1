8c23a719621ae009b9e91b01d1abdcaf3744bdc7
K.Bogatova <kris@yupi-bot.ru>
2024-02-20 19:14:52 +0300

    GitClone И внесены изменения components

--- movies_admin/config/components/checklist.py
+++ movies_admin/config/components/checklist.py
@@ -1,9 +1,9 @@
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (os.environ.get("SECRET_KEY"),)
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", False) == False
+DEBUG = os.environ.get("DEBUG", False) == True
 
-ALLOWED_HOSTS = ["127.0.0.1"]  # ['127.0.0.1']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(',')
 
 # INTERNAL_IPS = [
 #    '127.0.0.1',
